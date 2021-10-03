60aedad9cd09c322c2a4a5223f07e25719625e1d
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2021-10-03 23:44:37 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -12,7 +12,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>{OWNER}</a>\n○ Source Code : <a href='https://github.com/mrismanaziz/File-Sharing-Man'>Klik Disini</a>\n○ Channel : @Lunatic0de\n○ Support Group : @SharingUserbot</b>",
+            text=f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>{OWNER}</a>\n○ Source Code : <a href='https://github.com/mondar111/File-Sharing-Man'>Klik Disini</a>\n○ Channel : @familynvn\n○ Support Group : @Siniajaloh</b>",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
