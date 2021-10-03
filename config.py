646a147e0a8abcc67c5d2d3be7f7fd99af7f34be
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2021-10-03 23:50:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "ʀɪsᴍᴀɴ•")
+OWNER = os.environ.get("OWNER", "momdar")
 
 # Database
 DB_URI = os.environ.get("DATABASE_URL", "")
