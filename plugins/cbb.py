668b8fb6ec6bb6669eb94c6682b4c1de89a64a47
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2021-10-22 04:25:52 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -12,7 +12,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>{OWNER}</a>\n○ Source Code : <a href='t.me/familynvn'>Klik Disini</a>\n○ Channel : @donasibuas\n○ Support Group : @asupanbuas</b>",
+            text=f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>{OWNER}</a>\n○ channel mon : <a href='t.me/familynvn'>Klik Disini</a>\n○ Channel asupan : @donasibuas\n○ Support Group asupan : @asupanbuas</b>",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
